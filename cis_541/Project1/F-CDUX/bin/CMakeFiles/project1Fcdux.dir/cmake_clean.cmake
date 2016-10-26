58177433115122938cfbccb02b66ae171132483a
ayenpure <abhishek.yenpure@hotmail.com>
2016-10-26 09:23:18 -0700

    Begining work on 1FR

---
+++ cis_541/Project1/F-CDUX/bin/CMakeFiles/project1Fcdux.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project1Fcdux.dir/project1Fcdux.cxx.o"
+  "project1Fcdux.pdb"
+  "project1Fcdux"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project1Fcdux.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
