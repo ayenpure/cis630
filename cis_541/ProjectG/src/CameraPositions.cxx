e162a68ba3b2fc38a01f25974cdc2143aa47750b
ayenpure <abhishek.yenpure@hotmail.com>
2016-11-16 18:15:06 -0800

    camera position visualization

--- cis_541/ProjectG/src/CameraPositions.cxx
+++ cis_541/ProjectG/src/CameraPositions.cxx
@@ -44,117 +44,84 @@
 #include <string.h>
 #include <stdio.h>
 
-// Purpose: returns a 256x3 array of colors
-//
-/*unsigned char *
-GetColorMap(void) {
-	unsigned char controlPts[8][3] = { { 71, 71, 219 }, { 0, 0, 91 }, { 0, 255,
-			255 }, { 0, 127, 0 }, { 255, 255, 0 }, { 255, 96, 0 },
-			{ 107, 0, 0 }, { 224, 76, 76 }, };
-	int textureSize = 256;
-	unsigned char *ptr = new unsigned char[textureSize * 3];
-	int nControlPts = 8;
-	double amountPerPair = ((double) textureSize - 1.0) / (nControlPts - 1.0);
-	for (int i = 0; i < textureSize; i++) {
-		int lowerControlPt = (int) (i / amountPerPair);
-		int upperControlPt = lowerControlPt + 1;
-		if (upperControlPt >= nControlPts)
-			upperControlPt = lowerControlPt; // happens for i == textureSize-1
-
-		double proportion = (i / amountPerPair) - lowerControlPt;
-		for (int j = 0; j < 3; j++)
-			ptr[3 * i + j] = controlPts[lowerControlPt][j]
-					+ proportion
-							* (controlPts[upperControlPt][j]
-									- controlPts[lowerControlPt][j]);
-	}
-	return ptr;
-}*/
-
-using std::sin;
-using std::cos;
-using std::endl;
-using std::cout;
-using std::abs;
-
 #define X 'x'
 #define Y 'y'
 #define Z 'z'
 
 void rotate(double* camera_position, double angle, char axis,double* rotated_camera) {
-  double rotation_matrix[3][3];
-  if(axis == X) {
-    double x_rotation_matrix[3][3] = {
-      1,0,0,
-      0,cos(angle),-sin(angle),
-      0,sin(angle),cos(angle)
-    };
-    memcpy(rotation_matrix, x_rotation_matrix, 9*sizeof(double));
-  } else if (axis == Y) {
-    double y_rotation_matrix[3][3] = {
-      cos(angle),0,-sin(angle),
-      0,1,0,
-      sin(angle),0,cos(angle)
-    };
-    memcpy(rotation_matrix, y_rotation_matrix, 9*sizeof(double));
-  } else if (axis == Z) {
-    double z_rotation_matrix[3][3] = {
-      cos(angle),-sin(angle),0,
-      sin(angle),cos(angle),0,
-      0,0,1
-    };
-    memcpy(rotation_matrix, z_rotation_matrix, 9*sizeof(double));
-  }
-  /*for (int i = 0 ; i < 4 ; i++)
-  {
-      char str[256];
-      sprintf(str, "(%.7f %.7f %.7f)\n", rotation_matrix[i][0], rotation_matrix[i][1], rotation_matrix[i][2]);
-      cout << str;
-  }*/
-  for(int i = 0; i < 3; i++) {
-    rotated_camera[i] = 0;
-    for(int j = 0; j < 3; j++) {
-      rotated_camera[i] = rotated_camera[i] + camera_position[j]*rotation_matrix[j][i];
-    }
-    if(abs(rotated_camera[i]) < 0.0000001)
-      rotated_camera[i] = 0;
-  }
-  //cout << "rotated position {" << rotated_camera[0] << ", " << rotated_camera[1] << ", " << rotated_camera[2] << " }" << endl;
+	double rotation_matrix[3][3];
+	if(axis == X) {
+		double x_rotation_matrix[3][3] = {
+			1,0,0,
+			0,cos(angle),-sin(angle),
+			0,sin(angle),cos(angle)
+		};
+		memcpy(rotation_matrix, x_rotation_matrix, 9*sizeof(double));
+	} else if (axis == Y) {
+		double y_rotation_matrix[3][3] = {
+			cos(angle),0,-sin(angle),
+			0,1,0,
+			sin(angle),0,cos(angle)
+		};
+		memcpy(rotation_matrix, y_rotation_matrix, 9*sizeof(double));
+	} else if (axis == Z) {
+		double z_rotation_matrix[3][3] = {
+			cos(angle),-sin(angle),0,
+			sin(angle),cos(angle),0,
+			0,0,1
+		};
+		memcpy(rotation_matrix, z_rotation_matrix, 9*sizeof(double));
+	}
+	/*for (int i = 0 ; i < 4 ; i++)
+	{
+			char str[256];
+			sprintf(str, "(%.7f %.7f %.7f)\n", rotation_matrix[i][0], rotation_matrix[i][1], rotation_matrix[i][2]);
+			cout << str;
+	}*/
+	for(int i = 0; i < 3; i++) {
+		rotated_camera[i] = 0;
+		for(int j = 0; j < 3; j++) {
+			rotated_camera[i] = rotated_camera[i] + camera_position[j]*rotation_matrix[j][i];
+		}
+		if(abs(rotated_camera[i]) < 0.0000001)
+			rotated_camera[i] = 0;
+	}
+	//cout << "rotated position {" << rotated_camera[0] << ", " << rotated_camera[1] << ", " << rotated_camera[2] << " }" << endl;
 }
 
 void get_camera_positions(double (*camera_positions)[3]) {
-  double calc_camera_positions[8][16][3];
-  cout << "PI :" << M_PI << endl;
-  double first_rotation[16] = {
-    0, (M_PI/6), (M_PI/4), (M_PI/3),
-    (M_PI/2), (2*M_PI/3), (3*M_PI/4), (5*M_PI/6),
-    (M_PI), (7*M_PI/6), (5*M_PI/4), (4*M_PI/3),
-    (3*M_PI/2), (5*M_PI/3),(7*M_PI/4) ,(11*M_PI/6)
-  };
-  double second_rotation[7] = {
-    (M_PI/6), (M_PI/4), (M_PI/3), (M_PI/2), (2*M_PI/3), (3*M_PI/4), (5*M_PI/6)
-  };
-  double camera_position[3] = {40,0,0};
-  for(int i = 0; i < 16; i++) {
-    rotate(camera_position, first_rotation[i], Y, calc_camera_positions[0][i]);
-  };
-  for(int i = 1; i < 8; i++) {
-    for(int j = 0; j < 16; j++) {
-      rotate(calc_camera_positions[0][j], second_rotation[i-1], X, calc_camera_positions[i][j]);
-    }
-  };
-  int positions_count = 0;
-  for(int i = 0; i < 8 ; i++) {
-    for (int j = 0; j <16; j++) {
-      if(i > 0 && (j==0 || j==8))
-        continue;
-      camera_positions[positions_count][0] = calc_camera_positions[i][j][0];
-      camera_positions[positions_count][1] = calc_camera_positions[i][j][1];
-      camera_positions[positions_count][2] = calc_camera_positions[i][j][2];
-      positions_count++;
-    }
-  }
+	double calc_camera_positions[8][16][3];
+	double first_rotation[16] = {
+		0, (M_PI/6), (M_PI/4), (M_PI/3),
+		(M_PI/2), (2*M_PI/3), (3*M_PI/4), (5*M_PI/6),
+		(M_PI), (7*M_PI/6), (5*M_PI/4), (4*M_PI/3),
+		(3*M_PI/2), (5*M_PI/3),(7*M_PI/4) ,(11*M_PI/6)
+	};
+	double second_rotation[7] = {
+		(M_PI/6), (M_PI/4), (M_PI/3), (M_PI/2), (2*M_PI/3), (3*M_PI/4), (5*M_PI/6)
+	};
+	double camera_position[3] = {40,0,0};
+	for(int i = 0; i < 16; i++) {
+		rotate(camera_position, first_rotation[i], Y, calc_camera_positions[0][i]);
+	};
+	for(int i = 1; i < 8; i++) {
+		for(int j = 0; j < 16; j++) {
+			rotate(calc_camera_positions[0][j], second_rotation[i-1], X, calc_camera_positions[i][j]);
+		}
+	};
+	int positions_count = 0;
+	for(int i = 0; i < 8 ; i++) {
+		for (int j = 0; j <16; j++) {
+			if(i > 0 && (j==0 || j==8))
+				continue;
+			camera_positions[positions_count][0] = calc_camera_positions[i][j][0];
+			camera_positions[positions_count][1] = calc_camera_positions[i][j][1];
+			camera_positions[positions_count][2] = calc_camera_positions[i][j][2];
+			positions_count++;
+		}
+	}
 }
+
 
 class vtk441Mapper: public vtkOpenGLPolyDataMapper {
 protected:
@@ -213,75 +180,45 @@ public:
 	virtual void RenderPiece(vtkRenderer *ren, vtkActor *act) {
 		RemoveVTKOpenGLStateSideEffects();
 		SetupLight();
-		/*glEnable(GL_COLOR_MATERIAL);
-		 glClearColor(0.0, 0.0, 0.0, 0.0);
-		 glEnable(GL_DEPTH_TEST);
-		 glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4,
-		 0, 1, 12, 4, &ctrlpoints[0][0][0]);
-		 glEnable(GL_MAP2_VERTEX_3);
-		 glEnable(GL_AUTO_NORMAL);
-		 glMapGrid2f(20, 0.0, 1.0, 20, 0.0, 1.0);*/
-		/*int i, j;
-		 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		 glColor3f(1.0, 1.0, 1.0);
-		 glPushMatrix ();
-		 glRotatef(85.0, 1.0, 1.0, 1.0);
-		 for (j = 0; j <= 8; j++) {
-		 glBegin(GL_LINE_STRIP);
-		 for (i = 0; i <= 30; i++)
-		 glEvalCoord2f((GLfloat)i/30.0, (GLfloat)j/8.0);
-		 glEnd();
-		 glBegin(GL_LINE_STRIP);
-		 for (i = 0; i <= 30; i++)
-		 glEvalCoord2f((GLfloat)j/8.0, (GLfloat)i/30.0);
-		 glEnd();
-		 }
-		 glPopMatrix ();
-		 glFlush();*/
-		/*glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		 glPushMatrix();
-		 //glRotatef(85.0, 1.0, 1.0, 1.0);
-		 glEvalMesh2(GL_FILL, 0, 20, 0, 20);
-		 glPopMatrix();
-		 glFlush();*/
-		/* Enable evaluator */
-    double camera_positions[114][3];(camera_positions);
-    double focus[3];
-    for(int cam_index = 0; cam_index < 16; cam_index++) {
-      int focus_index = 0;
-      if (0) {
-        focus[0] = 0;
-  			focus[1] = 0;
-  			focus[2] = 0;
-  		} else if(1){
-  			//double focus[3] = {0,0,0};
-  			//camera = GetCamera(camera_positions[cam_index], focus);
-  			if (cam_index < 16)
-  				focus_index = (cam_index + 6) % 16;
-  			else if (1) {
-  				int quotient = (cam_index - 16) / 14;
-  				int pseudo_index = (cam_index - 16) % 14;
-  				int pseudo_focus_index = (pseudo_index + 5) % 14;
-  				focus_index = ((quotient * 14) + 16) + pseudo_focus_index;
-  			} else {
-  				int pseudo_index = cam_index + 56;
-  				if (pseudo_index >= 114) {
-  					focus_index = (pseudo_index % 114) + 16;
-  				} else {
-  					focus_index = pseudo_index;
-  				}
-  			}
-  			focus[0] = camera_positions[focus_index][0];
-  			focus[1] = camera_positions[focus_index][1];
-  			focus[2] = camera_positions[focus_index][2];
-  			//cout << file_index << ", " << cam_index << " : " << "{" << camera_positions[cam_index][0] << ", " << camera_positions[cam_index][1] << ", " << camera_positions[cam_index][2] << "} ";
-  			//cout << "{" << camera_positions[focus_index][0] << ", " << camera_positions[focus_index][1] << ", " << camera_positions[focus_index][2] << "}" << endl;
-  		}
-      glBegin(GL_LINES);
-      glVertex3f(camera_positions[cam_index][0],camera_positions[cam_index][1],camera_positions[cam_index][2]);
-      glVertex3f(focus[0],focus[1],focus[2]);
-      glEnd();
-    }
+		double camera_positions[114][3];
+		get_camera_positions(camera_positions);
+		for(int cam_index = 0; cam_index < 114; cam_index++) {
+			double focus[3];
+			int focus_index = 0;
+			if (0) {
+				focus[0] = 0;
+				focus[1] = 0;
+				focus[2] = 0;
+			} else if(1){
+				//double focus[3] = {0,0,0};
+				//camera = GetCamera(camera_positions[cam_index], focus);
+				if (cam_index < 16)
+					focus_index = (cam_index + 7) % 16;
+				else if (0) {
+					int quotient = (cam_index - 16) / 14;
+					int pseudo_index = (cam_index - 16) % 14;
+					int pseudo_focus_index = (pseudo_index + 6) % 14;
+					focus_index = ((quotient * 14) + 16) + pseudo_focus_index;
+				} else {
+					int pseudo_index = cam_index -16 + 70;
+					if (pseudo_index >= 98) {
+						focus_index = (pseudo_index % 98) + 7;
+					} else {
+						focus_index = pseudo_index;
+					}
+					focus_index+=16;
+				}
+				focus[0] = camera_positions[focus_index][0];
+				focus[1] = camera_positions[focus_index][1];
+				focus[2] = camera_positions[focus_index][2];
+			}
+			glBegin(GL_LINES);
+			glColor3ub(1,0,0);
+			glVertex3f(camera_positions[cam_index][0],camera_positions[cam_index][1],camera_positions[cam_index][2]);
+			glColor3ub(1,1,1);
+			glVertex3f(focus[0],focus[1],focus[2]);
+			glEnd();
+		}
 	}
 };
 
