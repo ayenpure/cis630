d1bd34fafadf5771bcf18ecd5743d830a592cf77
ayenpure <abhishek.yenpure@hotmail.com>
2016-12-03 20:07:32 -0800

    P541

--- cis_541/Project_541/src/project541.cxx
+++ cis_541/Project_541/src/project541.cxx
@@ -18,6 +18,8 @@
 using std::cerr;
 using std::endl;
 
+double camera_position[3] = {5,5,-20};
+
 double ceil441(double f) {
 	return ceil(f - 0.00001);
 }
@@ -35,11 +37,14 @@ double interpolate(double point_1, double point_2, double value_1,
 	return value_at_quest;
 }
 
+double vector_magnitude(double* quest_vec) {
+	return sqrt( (quest_vec[0] * quest_vec[0])
+						 + (quest_vec[1] * quest_vec[1])
+						 + (quest_vec[2] * quest_vec[2]));
+}
+
 void normalize_vector(double* quest_normal) {
-	double norm = sqrt(
-			(quest_normal[0] * quest_normal[0])
-					+ (quest_normal[1] * quest_normal[1])
-					+ (quest_normal[2] * quest_normal[2]));
+	double norm = vector_magnitude(quest_normal);
 	quest_normal[0] = quest_normal[0] / norm;
 	quest_normal[1] = quest_normal[1] / norm;
 	quest_normal[2] = quest_normal[2] / norm;
@@ -87,7 +92,6 @@ public:
 			double right_intercept, double current_x_pos,
 			double *color_at_left_intercept, double *color_at_right_intercept,
 			double *current_color) {
-
 		current_color[0] = interpolate(left_intercept, right_intercept,
 				color_at_left_intercept[0], color_at_right_intercept[0],
 				current_x_pos);
@@ -123,7 +127,7 @@ public:
 	double X[3];
 	double Y[3];
 	double Z[3];
-	double colors[3];
+	double colors[3][3];
 	double normal[3];
 
 
@@ -262,54 +266,62 @@ std::vector<Triangle> GetTriangles(const char *filename) {
 		tris[idx].calculate_normals();
 	}
 	return tris;*/
-	std::vector<Triangle> tris(3);
+	std::vector<Triangle> tris(5);
 	Triangle t1;
 	t1.X[0] = -5;t1.X[1] = -5;t1.X[2] = 5;
 	t1.Y[0] = -5;t1.Y[1] = 5;t1.Y[2] = -5;
 	t1.Z[0] = 0;t1.Z[1] = 0;t1.Z[2] = 0;
-	t1.colors[0] = 0;t1.colors[1] = 127;t1.colors[2] = 255;
+	t1.colors[0][0] = 0;t1.colors[0][1] = 0;t1.colors[0][2] = 255;
+	t1.colors[1][0] = 0;t1.colors[1][1] = 255;t1.colors[1][2] = 0;
+	t1.colors[2][0] = 255;t1.colors[2][1] = 0;t1.colors[2][2] = 0;
 	t1.calculate_normals();
 	tris[0] = t1;
 	Triangle t2;
-	t2.X[0] = -5;t2.X[1] = -5;t2.X[2] = 5;
-	t2.Y[0] = -5;t2.Y[1] = 5;t2.Y[2] = -5;
-	t2.Z[0] = 5;t2.Z[1] = 5;t2.Z[2] = 5;
-	t2.colors[0] = 255;t2.colors[1] = 127;t2.colors[2] = 0;
+	t2.X[0] = -2.5;t2.X[1] = -2.5;t2.X[2] = 2.5;
+	t2.Y[0] = -2.5;t2.Y[1] = 2.5;t2.Y[2] = -2.5;
+	t2.Z[0] = 10;t2.Z[1] = 10;t2.Z[2] = 10;
+	t2.colors[0][0] = 0;t2.colors[0][1] = 127;t2.colors[0][2] = 255;
+	t2.colors[1][0] = 0;t2.colors[1][1] = 127;t2.colors[1][2] = 255;
+	t2.colors[2][0] = 0;t2.colors[2][1] = 127;t2.colors[2][2] = 255;
 	t2.calculate_normals();
 	tris[1] = t2;
 	Triangle t3;
 	t3.X[0] = -5;t3.X[1] = -5;t3.X[2] = 5;
 	t3.Y[0] = -5;t3.Y[1] = 5;t3.Y[2] = -5;
 	t3.Z[0] = -5;t3.Z[1] = -5;t3.Z[2] = -5;
-	t3.colors[0] = 127;t3.colors[1] = 127;t3.colors[2] = 127;
+	t3.colors[0][0] = 255;t3.colors[0][1] = 127;t3.colors[0][2] = 0;
+	t3.colors[1][0] = 255;t3.colors[1][1] = 127;t3.colors[1][2] = 0;
+	t3.colors[2][0] = 255;t3.colors[2][1] = 127;t3.colors[2][2] = 0;
 	t3.calculate_normals();
 	tris[2] = t3;
+	Triangle t4;
+	t4.X[0] = -20;t4.X[1] = -20;t4.X[2] = 20;
+	t4.Y[0] = 10;t4.Y[1] = 10;t4.Y[2] = 10;
+	t4.Z[0] = -20;t4.Z[1] = 20;t4.Z[2] = 20;
+	t4.colors[0][0] = 255;t4.colors[0][1] = 255;t4.colors[0][2] = 255;
+	t4.colors[1][0] = 255;t4.colors[1][1] = 255;t4.colors[1][2] = 255;
+	t4.colors[2][0] = 255;t4.colors[2][1] = 255;t4.colors[2][2] = 255;
+	t4.calculate_normals();
+	tris[3] = t4;
+	Triangle t5;
+	t5.X[0] = -20;t5.X[1] = 20;t5.X[2] = 20;
+	t5.Y[0] = 10;t5.Y[1] = 10;t5.Y[2] = 10;
+	t5.Z[0] = -20;t5.Z[1] = -20;t5.Z[2] = 20;
+	t5.colors[0][0] = 255;t5.colors[0][1] = 255;t5.colors[0][2] = 255;
+	t5.colors[1][0] = 255;t5.colors[1][1] = 255;t5.colors[1][2] = 255;
+	t5.colors[2][0] = 255;t5.colors[2][1] = 255;t5.colors[2][2] = 255;
+	t5.calculate_normals();
+	tris[4] = t5;
 	return tris;
 }
 
-bool is_point_inside_triangle(double *point_of_intrsection, double *ray, Triangle triangle) {
+double calculate_area(double *edge_1, double *edge_2, double *area_vec) {
+	cross_product(edge_1,edge_2,area_vec);
+	double area = vector_magnitude(area_vec) / 2;
+}
 
-	if(triangle.X[0] == triangle.X[1] && triangle.X[1] == triangle.X[2] && triangle.X[2] == triangle.X[0]
-	&& triangle.Y[0] == triangle.Y[1] && triangle.Y[1] == triangle.Y[2] && triangle.Y[2] == triangle.Y[0]
-	&& triangle.Z[0] == triangle.Z[1] && triangle.Z[1] == triangle.Z[2] && triangle.Z[2] == triangle.Z[0]) {
-		/*if(point_of_intrsection[0] != triangle.X[0]
-		|| point_of_intrsection[1] != triangle.Y[0]
-		|| point_of_intrsection[2] != triangle.Z[0])
-			return false;*/
-		double camera_position[3] = {5,5,-20};
-		double point_vec[3] = {
-			triangle.X[0] - camera_position[0],
-			triangle.Y[0] - camera_position[1],
-			triangle.Z[0] - camera_position[2]
-		};
-		normalize_vector(point_vec);
-		if(point_vec[0] == ray[0]
-		&& point_vec[1] == ray[1]
-		&& point_vec[2] == ray[2])
-			return true;
-	}
-
-	double temp[3] = {0,0,0};
+bool is_point_inside_triangle(double *intersect_point, Triangle triangle, double *barycentric) {
+	/*double temp[3] = {0,0,0};
 	for(int i = 0; i < 3; i++) {
 		int adj_1 = (i + 1) % 3;
 		double vector_1[3] = {
@@ -318,53 +330,92 @@ bool is_point_inside_triangle(double *point_of_intrsection, double *ray, Triangl
 			triangle.Z[adj_1] -  triangle.Z[i]
 		};
 		double vector_2[3] = {
-			point_of_intrsection[0] - triangle.X[i],
-			point_of_intrsection[1] - triangle.Y[i],
-			point_of_intrsection[2] - triangle.Z[i]
+			intersect_point[0] - triangle.X[i],
+			intersect_point[1] - triangle.Y[i],
+			intersect_point[2] - triangle.Z[i]
 		};
 		cross_product(vector_1, vector_2, temp);
 		double dot = dot_product(triangle.normal, temp);
 		if(dot < 0)
 			return false;
 	}
+	return true;*/
+	double edge_1[3] = {
+		triangle.X[1] - triangle.X[0],
+		triangle.Y[1] - triangle.Y[0],
+		triangle.Z[1] - triangle.Z[0]
+	};
+	double edge_2[3] = {
+		triangle.X[2] - triangle.X[0],
+		triangle.Y[2] - triangle.Y[0],
+		triangle.Z[2] - triangle.Z[0]
+	};
+	double area_vec[3];
+	double t_area = calculate_area(edge_1, edge_2, area_vec);
+	//calculate barycentric coordinates
+	for(int i = 0; i < 3; i++) {
+		int adj = (i + 1) % 3;
+		edge_1[0] = triangle.X[adj] - triangle.X[i];
+		edge_1[1] = triangle.Y[adj] - triangle.Y[i];
+		edge_1[2] = triangle.Z[adj] - triangle.Z[i];
+
+		edge_2[0] = intersect_point[0] - triangle.X[i];
+		edge_2[1] = intersect_point[1] - triangle.Y[i];
+		edge_2[2] = intersect_point[2] - triangle.Z[i];
+		double area = calculate_area(edge_1, edge_2, area_vec);
+		if(dot_product(triangle.normal, area_vec) < 0)
+			return false;
+		barycentric[i] = area / t_area;
+	}
 	return true;
 }
 
+double calculate_shading(double (*colors)[3], double *barycentric, int component) {
+		if(colors[0][component] == colors[1][component]
+		&& colors[1][component] == colors[2][component]
+		&& colors[2][component] == colors[0][component])
+			return colors[0][component];
+		else
+			return barycentric[0]*colors[0][component]
+						+ barycentric[1]*colors[1][component]
+						+ barycentric[2]*colors[2][component];
+}
+
 void get_color_for_pixel(double *ray, std::vector<Triangle> triangles, double * color) {
-	double camera_position[3] = {5,5,-20};
-	//for(int i = 820; i < 821; i++) {
+	double distance = std::numeric_limits<double>::max();
 	for(int i = 0; i < triangles.size(); i++) {
-		if(dot_product(ray, triangles[i].normal) == 0)
-			cout << "";
-		else {
+		//Check if triangle is a single pixel triangle
+		//if not get point of intersection of the triangle with the triangle plane
+		if(dot_product(ray, triangles[i].normal) != 0) {
 			double triangle_vertex[3] = {
 				triangles[i].X[0],
 				triangles[i].Y[0],
 				triangles[i].Z[0]
 			};
-			double distance_form_origin = dot_product(triangles[i].normal,triangle_vertex);
+			double o_distance = dot_product(triangles[i].normal,triangle_vertex);
 			//float t = (dot(N, orig) + D) / dot(N, dir);
-			double distance_form_camera = - ((dot_product(triangles[i].normal, camera_position) + distance_form_origin) / dot_product(triangles[i].normal, ray));
+			double c_distance = - ((dot_product(triangles[i].normal, camera_position) + o_distance) / dot_product(triangles[i].normal, ray));
 			//Vec3f Phit = orig + t * dirtrina
-			double point_of_intrsection[3] = {
-				camera_position[0] + distance_form_camera*ray[0],
-				camera_position[1] + distance_form_camera*ray[1],
-				camera_position[2] + distance_form_camera*ray[2]
+			double intersect_point[3] = {
+				camera_position[0] + c_distance*ray[0],
+				camera_position[1] + c_distance*ray[1],
+				camera_position[2] + c_distance*ray[2]
 			};
-			if(is_point_inside_triangle(point_of_intrsection, ray, triangles[i])) {
-				cout << "Point is inside the triangle : " << i << endl;
-				color[0] = triangles[i].colors[0];
-				color[1] = triangles[i].colors[1];
-				color[2] = triangles[i].colors[2];
-			} /*else {
-				cout << "Point is outside the triangle" << endl;
-			}*/
+			double barycentric[3] = {0,0,0};
+			if(is_point_inside_triangle(intersect_point, triangles[i], barycentric)) {
+				if(c_distance < distance){
+					distance = c_distance;
+					color[0] = calculate_shading(triangles[i].colors, barycentric, 0);
+					color[1] = calculate_shading(triangles[i].colors, barycentric, 1);
+					color[2] = calculate_shading(triangles[i].colors, barycentric, 2);
+				}
+			}
 		}
 	}
 };
 
 int main() {
-	int height = 500,width = 500;
+	int height = 1000,width = 1000;
 	std::vector<Triangle> triangles = GetTriangles("hardyglobal.0.vtk");
 	cout << "Number of triangles " << triangles.size() << endl;
 	vtkImageData *image = NewImage(height, width);
@@ -376,7 +427,6 @@ int main() {
 	screen.buffer = buffer;
 	screen.width = height;
 	screen.height = width;
-	double camera_position[3] = {5,5,-20};
 	for(int x = 0; x < width; x++) {
 		for(int y = 0; y < height; y++) {
 			double translated_x = (20*(double)x)/(double)width - 10;
