3dc0479fabc9c5380d960f125271f798c6b516b2
ayenpure <abhishek.yenpure@hotmail.com>
2017-03-05 19:48:07 -0800

    Algorithms Assignment 5

--- CIS621/src/algos/MissionCritical.java
+++ CIS621/src/algos/MissionCritical.java
@@ -1,21 +1,26 @@
 package algos;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * CIS 621 : Algorithms and Complexity 
+ * Assignment 5
+ * @author Abhishek Yenpure
+ */
 class Machine {
-	private double cost;
+
+	private int cost;
 	private double reliability;
 
-	public double getCost() {
+	public int getCost() {
 		return cost;
 	}
 
-	public void setCost(double cost) {
+	public void setCost(int cost) {
 		this.cost = cost;
 	}
 
@@ -26,29 +31,21 @@ class Machine {
 	public void setReliability(double reliability) {
 		this.reliability = reliability;
 	}
-
 }
 
 public class MissionCritical {
 
 	private static final String BLANK = " ";
 
-	protected static double readInputFile(List<Machine> machines) {
-		double budget = 0;
-		FileReader reader = null;
-		try {
-			reader = new FileReader("inSample.txt");
-		} catch (FileNotFoundException e) {
-			System.err.println("Error occurred in reading the input file, " + e.getMessage());
-			System.exit(0);
-		}
-		BufferedReader buffReader = new BufferedReader(reader);
+	protected static int readInput(List<Machine> machines) {
+		int budget = 0;
+		BufferedReader buffReader = new BufferedReader(new InputStreamReader(System.in));
 		String line = null;
 		try {
 			int linecount = 0;
 			while ((line = buffReader.readLine()) != null) {
 				if (linecount == 0) {
-					budget = Double.parseDouble(line);
+					budget = Integer.parseInt(line);
 					linecount++;
 				} else if (linecount == 1) {
 					linecount++;
@@ -56,45 +53,122 @@ public class MissionCritical {
 				} else {
 					String split[] = line.split(BLANK);
 					Machine machine = new Machine();
-					machine.setCost(Double.parseDouble(split[0]));
+					machine.setCost(Integer.parseInt(split[0]));
 					machine.setReliability(Double.parseDouble(split[1]));
 					machines.add(machine);
 				}
 			}
 			buffReader.readLine();
 		} catch (IOException e) {
-			System.err.println("Error occurred in reading the input file, " + e.getMessage());
+			System.err.println("Error occurred while reading the input, " + e.getMessage());
 			System.exit(0);
 		}
 		return budget;
 	}
 
-	private static double getOptimumReliability(List<Machine> machines, double budget) {
-		if (machines.isEmpty())
-			return 1;
-		Machine current = machines.get(0);
-		if (budget < current.getCost())
+	private static void initSolutionSpace(double[][] sol, int[][] count, int size, int budget) {
+		for (int i = 0; i <= size; i++) {
+			for (int j = 0; j <= budget; j++) {
+				if (i == 0)
+					sol[i][j] = 1;
+				else if (j == 0)
+					sol[i][j] = 0;
+				else
+					sol[i][j] = -1;
+				count[i][j] = 0;
+			}
+		}
+	}
+
+	private static void printSolution(int[][] count, List<Machine> machines, int budget, double maxReliability) {
+		System.out.println("Maximum Reliability : " + maxReliability);
+		int machine = machines.size();
+		while (machine != 0) {
+			int current = count[machine][budget];
+			int currentCost = machines.get(machine - 1).getCost();
+			System.out.println(current + " copies of machine " + machine + " of cost " + currentCost);
+			budget -= current * currentCost;
+			machine--;
+		}
+	}
+
+	private static double getMaxReliabilityIterative(int budget, List<Machine> machines, double[][] sol,
+			int[][] count) {
+		for (int i = 1; i <= machines.size(); i++) {
+			Machine current = machines.get(i - 1);
+			for (int j = 1; j <= budget; j++) {
+				double maxReliability = 0;
+				for (int k = 1; k <= j / current.getCost(); k++) {
+					double currentReliability = sol[i - 1][j - k * current.getCost()]
+							* (1 - Math.pow(1 - current.getReliability(), k));
+					if (currentReliability > maxReliability) {
+						maxReliability = currentReliability;
+						sol[i][j] = maxReliability;
+						count[i][j] = k;
+					}
+				}
+			}
+		}
+		return sol[machines.size()][budget];
+	}
+
+	private static double getMaxReliabilityMemoized(int set, int budget, List<Machine> machines, double[][] sol,
+			int[][] count) {
+		if (budget < 0)
 			return 0;
-		int maxInstances = (int) (budget / current.getCost());
-		if(machines.size() == 1)
-			return (1 - Math.pow(1 - current.getReliability(), maxInstances));
+		else if (budget == 0 && set > 0)
+			return 0;
+		else if (budget >= 0 && set == 0)
+			return 1;
+		Machine current = machines.get(set - 1);
 		double maxReliability = 0;
-		List<Machine> remainingMachines = machines.subList(1, machines.size());
-		for (int instances = 1; instances <= maxInstances; instances++) {
-			if(machines.size() == 12)
-				System.out.println("Adding instance " + instances + " among " + maxInstances + " instances");				
-			double reliability = (1 - Math.pow(1 - current.getReliability(), instances))
-					* getOptimumReliability(remainingMachines, budget - instances * current.getCost());
-			if (reliability > maxReliability)
-				maxReliability = reliability;
+		for (int k = 1; k <= budget / current.getCost(); k++) {
+			if (sol[set - 1][budget - k * current.getCost()] == -1)
+				sol[set - 1][budget - k * current.getCost()] = getMaxReliabilityMemoized(set - 1,
+						budget - k * current.getCost(), machines, sol, count);
+			double currentReliability = sol[set - 1][budget - k * current.getCost()]
+					* (1 - Math.pow(1 - current.getReliability(), k));
+			if (currentReliability > maxReliability) {
+				maxReliability = currentReliability;
+				sol[set][budget] = maxReliability;
+				count[set][budget] = k;
+			}
 		}
 		return maxReliability;
 	}
 
+	private static void printMemoizationStatistics(double[][] sol, int size, int budget) {
+		System.out.println("\nMemoization Statistics :");
+		int total = (size + 1) * (budget + 1);
+		System.out.println("Total Locations : " + total);
+		int used = 0;
+		for (int i = 0; i <= size; i++) {
+			for (int j = 0; j <= budget; j++) {
+				if (sol[i][j] != -1)
+					used++;
+			}
+		}
+		System.out.println("Locations used : " + used);
+		System.out.println("Utilization % : " + (used / (double) total) * 100);
+	}
+
 	public static void main(String[] args) {
 		List<Machine> machines = new ArrayList<Machine>();
-		double budget = readInputFile(machines);
-		double reliability = getOptimumReliability(machines, budget);
-		System.out.println("Max reliability is " + reliability);
+		int budget = readInput(machines);
+		System.out.println("Budget : " + budget + "\nNumber of Machines : " + machines.size());
+		double sol[][] = new double[machines.size() + 1][budget + 1];
+		int count[][] = new int[machines.size() + 1][budget + 1];
+
+		double maxReliability;
+		System.out.println("\n\nIterated version :");
+		initSolutionSpace(sol, count, machines.size(), budget);
+		maxReliability = getMaxReliabilityIterative(budget, machines, sol, count);
+		printSolution(count, machines, budget, maxReliability);
+
+		System.out.println("\n\nMemoized version :");
+		initSolutionSpace(sol, count, machines.size(), budget);
+		maxReliability = getMaxReliabilityMemoized(machines.size(), budget, machines, sol, count);
+		printSolution(count, machines, budget, maxReliability);
+		printMemoizationStatistics(sol, machines.size(), budget);
 	}
 }
