68ab3b39980258fee305dc4ca73e2766dbf966af
ayenpure <abhishek.yenpure@hotmail.com>
2017-05-28 11:21:35 -0700

    peer

--- CIS630/project2/src/bullitenboard/Peer.java
+++ CIS630/project2/src/bullitenboard/Peer.java
@@ -7,43 +7,110 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
+/**
+ * Class representing a single Peer in the peer-to-peer ring based system.
+ * 
+ * @author abhishek
+ *
+ */
 public class Peer {
-	public static void main(String[] args) {
-		DatagramSocket socket = null;
+
+	private DatagramSocket socket;
+	
+	public Peer() {
+		int sPort = 4445;
 		try {
-			socket = new DatagramSocket();
+			socket = new DatagramSocket(sPort);
 		} catch (SocketException e) {
-			System.err.println("Failed to create requesting socket");
+			System.err.println("Failed to create socket with port " + sPort);
 			System.exit(1);
 		}
-		byte[] buf = new byte[256];
-        InetAddress address = null;
-		try {
-			address = InetAddress.getLocalHost();
-		} catch (UnknownHostException e) {
-			System.err.println("Failed to get address for the serving peer");
-			System.exit(1);
-		}
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
-        try {
-			socket.send(packet);
-		} catch (IOException e) {
-			System.err.println("Failed to send request to the serving peer");
-			System.exit(1);
-		}
-    
-            // get response
-        packet = new DatagramPacket(buf, buf.length);
-        try {
-			socket.receive(packet);
-		} catch (IOException e) {
-			System.err.println("Failed to receive response from the serving peer");
-			System.exit(1);
+	}
+	
+	class Receive extends Thread {
+		private boolean status = true;
+
+		public void setStatus(boolean status) {
+			this.status = status;
 		}
 
-	    // display response
-        String received = new String(packet.getData(), 0, packet.getLength());
-        System.out.println("Quote of the Moment: " + received);
-        socket.close();
-	}
-}
+		@Override
+		public void run() {
+			System.out.println("Client is up");
+			byte buffer[] = new byte[256];
+			while (status) {
+				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+				try {
+					socket.receive(packet);
+					System.out.println("Client request reads : " + new String(packet.getData(), 0, packet.getLength()));
+				} catch (IOException e) {
+					System.err.println("Failed to receive request from client");
+					System.exit(1);
+				}
+				String toSend = "Hello!! from the serving socket";
+				InetAddress address = packet.getAddress();
+				int dPort = packet.getPort();
+				buffer = toSend.getBytes();
+				packet = new DatagramPacket(buffer, buffer.length, address, dPort);
+				try {
+					socket.send(packet);
+				} catch (IOException e) {
+					System.err.println("Failed to respond to client request");
+					System.exit(1);
+				}
+			}
+		}
+	};
+
+	class Send extends Thread {
+		@Override
+		public void run() {
+			String request = "Hello!! from the requesting socket";
+			byte buffer[] = request.getBytes();
+			InetAddress address = null;
+			try {
+				address = InetAddress.getLocalHost();
+			} catch (UnknownHostException e) {
+				System.err.println("Failed to get address for the serving peer");
+				System.exit(1);
+			}
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 4445);
+			try {
+				socket.send(packet);
+			} catch (IOException e) {
+				System.err.println("Failed to send request to the serving peer");
+				System.exit(1);
+			}
+
+			// get response
+			packet = new DatagramPacket(buffer, buffer.length);
+			try {
+				socket.receive(packet);
+			} catch (IOException e) {
+				System.err.println("Failed to receive response from the serving peer");
+				System.exit(1);
+			}
+
+			// display response
+			String received = new String(packet.getData(), 0, packet.getLength());
+			System.out.println("Quote of the Moment: " + received);
+			//socket.close();
+		};
+	};
+
+	public void send() {
+		Send send = new Send();
+		send.start();
+	};
+	
+	public void receive() {
+		Receive receive = new Receive();
+		receive.start();
+	};
+	
+	public static void main(String[] args) {
+		Peer peer = new Peer();
+		peer.receive();
+		peer.send();
+	};
+};
