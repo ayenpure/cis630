767627254cdabe85342aaa13e3fc58fec39f6495
ayenpure <abhishek.yenpure@hotmail.com>
2017-06-04 16:53:31 -0700

    updated peer

--- CIS630/project2/src/bullitenboard/Peer.java
+++ CIS630/project2/src/bullitenboard/Peer.java
@@ -10,10 +10,14 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.swing.text.html.HTMLDocument.HTMLReader.SpecialAction;
+import java.util.NavigableMap;
+import java.util.TimeZone;
+import java.util.TreeMap;
 
 /**
  * Class representing a single Peer in the peer-to-peer ring based system.
@@ -21,28 +25,91 @@ import javax.swing.text.html.HTMLDocument.HTMLReader.SpecialAction;
  * @author abhishek
  */
 public class Peer {
-	// private static final String TOKEN = "TOKEN";
+	private static final String TOKEN = "TOKEN";
+	private static final String PROBE = "PROBE";
+	private static final String PROBE_REPLY = "PROBE-REPLY";
 
+	private static final String INPUT = "-i";
 	private static final String CONFIG = "-c";
 	private DatagramSocket socket;
+	private int port;
 	private int rangeStart, rangeEnd;
 	private int nextHop, previousHop;
-	private int port;
+	private long goLiveTime;
+	private long join, leave;
+	private long probetime;
 	private boolean alive;
+	private Receive listener;
+	private NavigableMap<Long, String> messages;
 
-	public Peer(int port, int rangeStart, int rangeEnd) throws PeerException {
+	public long getJoin() {
+		return join;
+	}
+
+	public void setJoin(long join) {
+		this.join = join;
+	}
+	
+	public long getLeave() {
+		return leave;
+	}
+
+	public void setLeave(long leave) {
+		this.leave = leave;
+	}
+
+	public long getGoLiveTime() {
+		return goLiveTime;
+	}
+
+	public void setGoLiveTime(long goLiveTime) {
+		
+		this.goLiveTime = goLiveTime;
+	}
+	
+	private void setMessages(NavigableMap<Long, String> messages) {
+		this.messages = messages;
+	}
+
+	public Peer(int port, int rangeStart, int rangeEnd, long join, long leave) throws PeerException {
 		this.port = port;
 		this.rangeStart = rangeStart;
 		this.rangeEnd = rangeEnd;
+		this.join = join;
+		this.leave = leave;
 		this.nextHop = (port == rangeStart) ? rangeEnd : rangeStart;
 		this.previousHop = -1;
-		System.out.println("Port : " + port);
+		System.out.println("Port : " + this.port);
 		System.out.println("Next hop : " + nextHop);
 		this.setAlive(false);
 		try {
-			socket = new DatagramSocket(port);
+			socket = new DatagramSocket(this.port);
 		} catch (SocketException e) {
 			throw new PeerException("Failed to create socket with port " + port);
+		}
+	}
+	
+	private void sendMessages(long receivedTimestamp) {
+		NavigableMap<Long, String> headMap = messages.headMap(receivedTimestamp, true);
+		while (!headMap.isEmpty()) {
+			Long firstKey = headMap.firstKey();
+			String message = headMap.get(firstKey);
+			
+			
+			/*SimpleDateFormat formatter = new SimpleDateFormat("mm:ss");
+			formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+			System.out.println(formatter.format(new Date(firstKey)) + " : " + message);*/
+			
+			//TODO: Send all these messages
+			
+			headMap.remove(firstKey);
+			messages.remove(firstKey);
+		}
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			System.err.println("Sleep error");
+			System.exit(1);
 		}
 	}
 
@@ -55,19 +122,48 @@ public class Peer {
 					byte buffer[] = new byte[256];
 					DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 					socket.receive(packet);
-					System.out
-							.println("Received message reads : " + new String(packet.getData(), 0, packet.getLength()));
+					long receivedTimestamp = System.currentTimeMillis();
+					int senderPort = packet.getPort();
+					String message = new String(packet.getData(), 0, packet.getLength());
+					if(message.equals(PROBE)){
+						if(senderPort > previousHop)
+							previousHop = senderPort;
+						//TODO: Reply to probe
+					} else if (message.equals(PROBE_REPLY)) {
+						if(receivedTimestamp - probetime < 1000) {
+							nextHop = senderPort;
+						}
+					} else if(message.equals(TOKEN)) {
+						if(nextHop == -1)
+							continue;
+						if(senderPort == previousHop){
+							if(messages.isEmpty()) {
+								//TODO: Forward token
+							} else {
+								sendMessages(receivedTimestamp);
+							}
+						}
+					} else {
+						if(nextHop == -1)
+							continue;
+						if(senderPort == previousHop){
+							//TODO: Forward Message
+						}
+					}
+					//System.out.println("Received message reads : " + new String(packet.getData(), 0, packet.getLength()));
 				} catch (IOException e) {
 					System.err.println("There was a problem while receiving server request :" + e.getMessage());
 					socket.close();
 					System.exit(1);
 				}
 			}
-			socket.close();
+			//socket.close();
 		}
 	};
 
 	public void send(String message) throws PeerException {
+		if(!isAlive())
+			return;
 		byte buffer[] = message.getBytes();
 		InetAddress address = null;
 		try {
@@ -84,8 +180,8 @@ public class Peer {
 	};
 
 	public void receive() {
-		Receive receive = new Receive();
-		receive.start();
+		listener = new Receive();;
+		listener.start();
 	};
 
 	public boolean isAlive() {
@@ -93,6 +189,8 @@ public class Peer {
 	}
 
 	public void setAlive(boolean alive) {
+		if(alive)
+			System.out.println("Peer " + port + " is alive");
 		this.alive = alive;
 	}
 
@@ -103,22 +201,117 @@ public class Peer {
 		argMap.put(args[2], args[3]);
 		argMap.put(args[4], args[5]);
 
+		
+		// Add code to halt the thread until it's time.
+		
+		Peer peer = null;
+		NavigableMap<Long, String> messages = null;
+		try {
+			peer = constructPeer(argMap.get(CONFIG));
+			messages = getMessageMap(argMap.get(INPUT));
+		} catch (IOException | ParseException e) {
+			
+		}
+		if(peer == null || messages == null) {
+			System.err.println("System might be in an inconsistent state. "
+					+ "Either the peer of the messages were not initialized");
+			System.exit(1);
+		}
+		peer.setGoLiveTime(System.currentTimeMillis());
+		peer.setMessages(messages);
+		schedule(peer);
+	}
+
+	private static void schedule(Peer peer) {
+		System.out.println("Scheduling peer" + peer.port);
+		long goLive = peer.getGoLiveTime();
+		long join = peer.getJoin();
+		long leave = peer.getLeave();
+		System.out.println("waiting to join");
+		while((System.currentTimeMillis()) - goLive < join) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				System.err.println("Sleep Error");
+				System.exit(1);
+			}
+			
+		}
+		System.out.println("joining");
+		peer.setAlive(true);
+		peer.receive();
+		System.out.println("waiting to leave");
+		while((System.currentTimeMillis()) - goLive < leave) {			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				System.err.println("Sleep Error");
+				System.exit(1);
+			}
+		}
+		System.out.println("leaving");
+		peer.setAlive(false);
+		peer.listener.stop();
+		peer.socket.close();
+	}
+
+	private static NavigableMap<Long, String> getMessageMap(String fileName) throws IOException, ParseException {
+		NavigableMap<Long, String> messages = new TreeMap<Long, String>();
+		File input;
+		FileReader reader = null;
+		BufferedReader buffReader = null;
+		input = new File(fileName);
+		try {
+			reader = new FileReader(input);
+		} catch (FileNotFoundException e) {
+			System.err.println("Failed to read config file");
+			System.exit(1);
+		}
+		if(reader != null)
+			buffReader = new BufferedReader(reader);
+		else {
+			System.err.println("Error occured while reading config file");
+			System.exit(1);
+		}
+		SimpleDateFormat formatter = new SimpleDateFormat("mm:ss");
+		formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+		String line;
+		while ((line = buffReader.readLine()) != null) {
+			if(line.trim().isEmpty())
+				continue;
+			String[] split = line.split("\t");
+			long timestamp = 0;
+			timestamp = formatter.parse(split[0].trim()).getTime();
+			messages.put(timestamp , split[1].trim());
+		}	
+		return messages;
+	}
+
+	private static Peer constructPeer(String fileName) {
 		Peer peer = null;
 		File config;
 		FileReader reader = null;
-		BufferedReader buffReader;
+		BufferedReader buffReader = null;
 
-		config = new File(argMap.get(CONFIG));
+		config = new File(fileName);
 		try {
 			reader = new FileReader(config);
 		} catch (FileNotFoundException e) {
 			System.err.println("Failed to read config file");
+			System.exit(1);
 		}
-		buffReader = new BufferedReader(reader);
-
+		if(reader != null)
+			buffReader = new BufferedReader(reader);
+		else {
+			System.err.println("Error occured while reading config file");
+			System.exit(1);
+		}
+		SimpleDateFormat formatter = new SimpleDateFormat("mm:ss");
+		formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
 		String line;
 		try {
 			int port = 0, rangeStart = 0, rangeEnd = 0;
+			long join = 0, leave = 0;
 			while ((line = buffReader.readLine()) != null) {
 				if (line.contains("client_port")) {
 					String[] split = line.split(":");
@@ -129,38 +322,28 @@ public class Peer {
 					String[] split = line.split(":");
 					port = Integer.parseInt(split[1].trim());
 				} else if (line.contains("join_time")) {
-
+					String[] split = line.split(":", 2);
+					join = formatter.parse(split[1].trim()).getTime();
 				} else if (line.contains("leave_time")) {
-
+					String[] split = line.split(":", 2);
+					leave = formatter.parse(split[1].trim()).getTime();
 				}
 			}
-			peer = new Peer(port, rangeStart, rangeEnd);
-		} catch (NumberFormatException | IOException | PeerException e) {
+			peer = new Peer(port, rangeStart, rangeEnd, join, leave);
+		} catch (NumberFormatException | IOException | PeerException | ParseException e) {
 			System.err.println("Error occured while reading config file" + e.getMessage());
 			if (peer != null)
 				peer.socket.close();
 			System.exit(1);
 		}
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			System.err.println("Error occurred while waiting");
-			peer.socket.close();
-			System.exit(1);
-		}
-		peer.setAlive(true);
-		peer.receive();
-		try {
-			peer.send("");
-		} catch (PeerException e) {
-			System.err.println("Error occured while reading config file" + e.getMessage());
-			peer.socket.close();
-			System.exit(1);
-		}
+		return peer;
 	}
 };
 
 class PeerException extends Exception {
+	
+	private static final long serialVersionUID = -1165295127240695115L;
+
 	public PeerException(String message) {
 		super(message);
 	}
