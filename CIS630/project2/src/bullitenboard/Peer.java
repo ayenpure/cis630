ddf66de9823ccbe95bf3051c01dfb6d666ad74df
ayenpure <abhishek.yenpure@hotmail.com>
2017-06-04 09:06:45 -0700

    CIS 630 project 2 changes

--- CIS630/project2/src/bullitenboard/Peer.java
+++ CIS630/project2/src/bullitenboard/Peer.java
@@ -10,149 +10,158 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.SpecialAction;
 
 /**
  * Class representing a single Peer in the peer-to-peer ring based system.
+ * 
  * @author abhishek
  */
 public class Peer {
-	private static final String TOKEN = "TOKEN";
-	
+	// private static final String TOKEN = "TOKEN";
+
+	private static final String CONFIG = "-c";
 	private DatagramSocket socket;
-	int rangeStart, rangeEnd;
-	int neighbor;
-	int port;
-	boolean alive;
-	
-	public Peer(int port, int rangeStart, int rangeEnd) {
+	private int rangeStart, rangeEnd;
+	private int nextHop, previousHop;
+	private int port;
+	private boolean alive;
+
+	public Peer(int port, int rangeStart, int rangeEnd) throws PeerException {
 		this.port = port;
 		this.rangeStart = rangeStart;
 		this.rangeEnd = rangeEnd;
-		this.neighbor = -1;
-		this.alive = false;
+		this.nextHop = (port == rangeStart) ? rangeEnd : rangeStart;
+		this.previousHop = -1;
+		System.out.println("Port : " + port);
+		System.out.println("Next hop : " + nextHop);
+		this.setAlive(false);
 		try {
 			socket = new DatagramSocket(port);
 		} catch (SocketException e) {
-			System.err.println("Failed to create socket with port " + port);
-			System.exit(1);
+			throw new PeerException("Failed to create socket with port " + port);
 		}
 	}
-	
+
 	class Receive extends Thread {
 		@Override
 		public void run() {
-			System.out.println("Client is up");
-			byte buffer[] = new byte[256];
-			while (Peer.this.alive) {
-				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+			System.out.println("Receive listener is up");
+			while (Peer.this.isAlive()) {
 				try {
+					byte buffer[] = new byte[256];
+					DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 					socket.receive(packet);
-					System.out.println("Client request reads : " + new String(packet.getData(), 0, packet.getLength()));
-				} catch(SocketException e) {
-					System.err.println("The socket reached a timeout while discovering another node");
-					System.exit(1);
+					System.out
+							.println("Received message reads : " + new String(packet.getData(), 0, packet.getLength()));
 				} catch (IOException e) {
-					System.err.println("Failed to receive request from client");
-					System.exit(1);
-				}
-				String toSend = "Hello!! from the serving socket";
-				InetAddress address = packet.getAddress();
-				int dPort = packet.getPort();
-				buffer = toSend.getBytes();
-				packet = new DatagramPacket(buffer, buffer.length, address, dPort);
-				try {
-					socket.send(packet);
-				} catch (IOException e) {
-					System.err.println("Failed to respond to client request");
+					System.err.println("There was a problem while receiving server request :" + e.getMessage());
+					socket.close();
 					System.exit(1);
 				}
 			}
+			socket.close();
 		}
 	};
 
-	class Send extends Thread {
-		@Override
-		public void run() {
-			while(Peer.this.alive) {
-				String request = "Hello!! from the requesting socket";
-				byte buffer[] = request.getBytes();
-				InetAddress address = null;
-				try {
-					address = InetAddress.getLocalHost();
-				} catch (UnknownHostException e) {
-					System.err.println("Failed to get address for the serving peer");
-					System.exit(1);
-				}
-				DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 4445);
-				try {
-					socket.send(packet);
-				} catch (IOException e) {
-					System.err.println("Failed to send request to the serving peer");
-					System.exit(1);
-				}
-				
-				// get response
-				packet = new DatagramPacket(buffer, buffer.length);
-				try {
-					socket.receive(packet);
-				} catch (IOException e) {
-					System.err.println("Failed to receive response from the serving peer");
-					System.exit(1);
-				}
-				
-				// display response
-				String received = new String(packet.getData(), 0, packet.getLength());
-				System.out.println("Quote of the Moment: " + received);
-			}
-			//socket.close();
-		};
+	public void send(String message) throws PeerException {
+		byte buffer[] = message.getBytes();
+		InetAddress address = null;
+		try {
+			address = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			throw new PeerException("Failed to get address for the serving peer : " + e.getMessage());
+		}
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, nextHop);
+		try {
+			socket.send(packet);
+		} catch (IOException e) {
+			throw new PeerException("Failed to send request to the serving peer" + e.getMessage());
+		}
 	};
-	
-	public void send() {
-		Send send = new Send();
-		send.start();
-	};
-	
+
 	public void receive() {
 		Receive receive = new Receive();
 		receive.start();
 	};
-	
+
+	public boolean isAlive() {
+		return alive;
+	}
+
+	public void setAlive(boolean alive) {
+		this.alive = alive;
+	}
+
 	public static void main(String[] args) {
-		Peer peer;
+
+		Map<String, String> argMap = new HashMap<String, String>();
+		argMap.put(args[0], args[1]);
+		argMap.put(args[2], args[3]);
+		argMap.put(args[4], args[5]);
+
+		Peer peer = null;
 		File config;
 		FileReader reader = null;
 		BufferedReader buffReader;
-		
-		config = new File(args[0]);
+
+		config = new File(argMap.get(CONFIG));
 		try {
 			reader = new FileReader(config);
 		} catch (FileNotFoundException e) {
 			System.err.println("Failed to read config file");
-			System.exit(1);
 		}
 		buffReader = new BufferedReader(reader);
-		
+
 		String line;
 		try {
-			int port, rangeStart,rangeEnd;
-			while((line = buffReader.readLine()) != null) {
-				if(line.contains("client_port")) {
-					
+			int port = 0, rangeStart = 0, rangeEnd = 0;
+			while ((line = buffReader.readLine()) != null) {
+				if (line.contains("client_port")) {
+					String[] split = line.split(":");
+					String[] range = split[1].split("-");
+					rangeStart = Integer.parseInt(range[0].trim());
+					rangeEnd = Integer.parseInt(range[1].trim());
 				} else if (line.contains("my_port")) {
 					String[] split = line.split(":");
 					port = Integer.parseInt(split[1].trim());
 				} else if (line.contains("join_time")) {
-					
+
 				} else if (line.contains("leave_time")) {
-					
+
 				}
 			}
-			peer = new Peer(Integer.parseInt(args[0]), 4555, 4556);
-		} catch (NumberFormatException | IOException e) {
-			System.err.println("Error occured while reading config file");
+			peer = new Peer(port, rangeStart, rangeEnd);
+		} catch (NumberFormatException | IOException | PeerException e) {
+			System.err.println("Error occured while reading config file" + e.getMessage());
+			if (peer != null)
+				peer.socket.close();
+			System.exit(1);
+		}
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			System.err.println("Error occurred while waiting");
+			peer.socket.close();
+			System.exit(1);
+		}
+		peer.setAlive(true);
+		peer.receive();
+		try {
+			peer.send("");
+		} catch (PeerException e) {
+			System.err.println("Error occured while reading config file" + e.getMessage());
+			peer.socket.close();
 			System.exit(1);
 		}
 	}
+};
 
+class PeerException extends Exception {
+	public PeerException(String message) {
+		super(message);
+	}
 };
