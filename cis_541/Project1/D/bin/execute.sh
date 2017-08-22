b9efaf8c7b7f2c973aa9d62e9cc174ca6402fd2e
ayenpure <abhishek.yenpure@hotmail.com>
2017-08-21 22:53:11 -0400

    Moving DS to new repo

--- cis_541/Project1/D/bin/execute.sh
+++
@@ -1,5 +0,0 @@
-begin=$(date +%s%N)
-./project1D
-end=$(date +%s%N)
-tottime=$(expr $end - $begin)
-echo $(expr $tottime / 1000000) ms
--- cis_541/Project1/D/src/execute.sh
+++
@@ -1,5 +0,0 @@
-begin=$(date +%s)
-./project1D
-end=$(date +%s)
-tottime=$(expr $end - $begin)
-echo $tottime
